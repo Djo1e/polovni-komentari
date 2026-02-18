50377c640c90c045b85d956eb2b3674fabf43994
Djordje <djordjespasic7@gmail.com>
2026-02-18 17:19:57 +0100

    chore: ignore compiled JS artifacts and tsconfig build info

--- src/utils/anonymousId.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = "anonymousId";
-export async function getOrCreateAnonymousId() {
-    const result = await chrome.storage.local.get(STORAGE_KEY);
-    if (result[STORAGE_KEY])
-        return result[STORAGE_KEY];
-    const id = crypto.randomUUID();
-    await chrome.storage.local.set({ [STORAGE_KEY]: id });
-    return id;
-}
--- src/utils/listingId.js
+++
@@ -1,4 +0,0 @@
-export function extractListingId(url) {
-    const match = /\/auto-oglasi\/oglas\/[^/]+-(\d+)\.html/.exec(url);
-    return match ? match[1] : null;
-}
